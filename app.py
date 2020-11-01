a63b0c56b53dda20ad38c83239aedd15ccb812d1
MariaDanae <maria.d.papadopoulos@gmail.com>
2020-11-01 11:07:58 -0500

    Base Model with split test/train and accuracy

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 from flask import Flask
 
+from src.IO.get_data_from_yahoo import get_last_close_price
 from src.business_logic.process_query import create_business_logic
 
 app = Flask(__name__)
 
+#Linked to GCP project called 'stock'
 
 @app.route('/', methods=['GET'])
 def hello():
     return f'Hello dear students, you should use an other route:!\nEX: get_stock_val/<ticker>\n'
 
+# @app.route('/get_stock_val/<ticker>', methods=['GET'])
+# def get_stock_value(ticker):
+#     bl = create_business_logic()
+#     prediction = bl.do_predictions_for(ticker)
+#
+#     return f'{prediction}\n'
+
 
 @app.route('/get_stock_val/<ticker>', methods=['GET'])
 def get_stock_value(ticker):
     bl = create_business_logic()
-    prediction = bl.do_predictions_for(ticker)
+    prediction_and_score = bl.do_predictions_for(ticker)
+    last_close_price = get_last_close_price(ticker)
 
-    return f'{prediction}\n'
+    result = {"ticker": f'{ticker}',
+              "last_close_price": f'{last_close_price.get("close")}',
+              "prediction": f'{prediction_and_score.get("prediction")}',
+              "balanced_accuracy": f'{prediction_and_score.get("balanced_accuracy")}'
+              }
+    return f'{result}'
 
 
 if __name__ == '__main__':
--- src/algo/dummy_model.py
+++ src/algo/dummy_model.py
@@ -1,25 +1,59 @@
 import logging
+from turtle import pd
 
+import pandas
+from pandas import np
+from sklearn import preprocessing
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import balanced_accuracy_score, precision_score
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeRegressor
 
 
 def create_features(df_stock, nlags=10):
-    df_resampled = df_stock.copy()
-    lags_col_names = []
-    for i in range(nlags + 1):
-        df_resampled['lags_' + str(i)] = df_resampled['close'].shift(i)
-        lags_col_names.append('lags_' + str(i))
-    df = df_resampled[lags_col_names]
-    print(df)
-    df = df.dropna(axis=0)
+    # df_resampled = df_stock.copy()
+    # lags_col_names = []
+    # for i in range(nlags + 1):
+    #     df_resampled['lags_' + str(i)] = df_resampled['close'].shift(i)
+    #     lags_col_names.append('lags_' + str(i))
+    # df = df_resampled[lags_col_names]
+    # print(df)
+    # df = df.dropna(axis=0)
+
+    # sort data by date
+    df = df_stock.rename_axis('date').reset_index()
+    df = df.sort_values(by=['date'])
+
+    # create buy/sell column to be used in classification
+    df['buysell'] = 1
+    for index, row in df.iterrows():
+        if index == 0:
+            continue
+        if df['close'][index - 1] < df['close'][index]:
+            df['buysell'][index - 1] = 1  # buy
+        else:
+            df['buysell'][index - 1] = 0  # sell
+    # remove last row since we don't know for tomrw
+    df = df.head(-1)
+    df['year'] = pandas.DatetimeIndex(df['date']).year
+    df['month'] = pandas.DatetimeIndex(df['date']).month
+    df['day'] = pandas.DatetimeIndex(df['date']).day
+    df['weekday_name'] = df.date.dt.day_name()
+    df = df.set_index('date')
+    df = df.drop('ticker', 1)
+
+    label_encoder = preprocessing.LabelEncoder()
+    df['weekday_name'] = label_encoder.fit_transform(df['weekday_name'])
 
     return df
 
 
-def create_X_Y(df_lags):
-    X = df_lags.drop('lags_0', axis=1)
-    Y = df_lags[['lags_0']]
+def create_X_Y(df_lags, y_col):
+    X = df_lags.drop(y_col, axis=1)
+    Y = df_lags[[y_col]]
     return X, Y
 
 
@@ -28,23 +62,60 @@ class Stock_model(BaseEstimator, TransformerMixin):
     def __init__(self, data_fetcher):
         self.log = logging.getLogger()
         self.lr = LinearRegression()
+        self.rfc = RandomForestClassifier(n_estimators=100)
         self._data_fetcher = data_fetcher
         self.log.warning('here')
 
     def fit(self, X, Y=None):
         data = self._data_fetcher(X)
         df_features = create_features(data)
-        df_features, Y = create_X_Y(df_features)
-        self.lr.fit(df_features, Y)
+        df_features, Y = create_X_Y(df_features, 'buysell')
+        self.rfc.fit(df_features, Y)
         return self
 
     def predict(self, X, Y=None):
-        print(X)
+        results = {}
+        print(f'Get data for {X}')
         data = self._data_fetcher(X, last=True)
-        print(data)
-        df_features = create_features(data)
-        print(df_features)
-        df_features, Y = create_X_Y(df_features)
-        predictions = self.lr.predict(df_features)
+        print(f'Data:\n {data}')
 
-        return predictions.flatten()[-1]
+        # sort data by date
+        df = data.rename_axis('date').reset_index()
+        df = df.sort_values(by=['date'])
+
+        # create buy/sell column to be used in classification
+        df['buysell'] = 1
+        for index, row in df.iterrows():
+            if index == 0:
+                continue
+            if df['close'][index-1] < df['close'][index]:
+                df['buysell'][index-1] = 1 #buy
+            else:
+                df['buysell'][index-1] = 0 #sell
+        # remove last row since we don't know for tomrw
+        df = df.head(-1)
+        df['year'] = pandas.DatetimeIndex(df['date']).year
+        df['month'] = pandas.DatetimeIndex(df['date']).month
+        df['day'] = pandas.DatetimeIndex(df['date']).day
+        df['weekday_name'] = df.date.dt.day_name()
+        df = df.set_index('date')
+        df = df.drop('ticker', 1)
+        label_encoder = preprocessing.LabelEncoder()
+        df['weekday_name'] = label_encoder.fit_transform(df['weekday_name'])
+
+        X, Y = create_X_Y(df, 'buysell')
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+        clf = RandomForestClassifier(max_depth=2) #, max_features=2)
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+
+        results["balanced_accuracy"] = balanced_accuracy_score(y_test, y_pred)
+        # results["important_features"] = pd.Series(clf.feature_importances_, index=iris.feature_names).sort_values(ascending=False)
+
+        prediction = clf.predict(X_train)
+        prediction = np.where(prediction == 1, "Buy", "Sell")
+        results["prediction"] = prediction.flatten()[-1]
+
+        return results
